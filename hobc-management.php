bc99da0f127463239698b30905e954bf722e2d36
Rubel Mahmud <rubel601619@gmail.com>
2025-05-31 12:00:27 +0600

    create the plugin initiate file.

---
+++ hobc-management.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: HOBC Management
+Plugin URI: https://github.com/rubel601619/hobc-management
+Description: Manage and organize badminton players, profiles, rankings, and matches within WordPress.
+Version: 1.0.0
+Author: Rubel Mahmud (Sujan)
+Author URI: https://velixtech.com
+License: GPL2
+Text Domain: hobc-management
+Domain Path: /lang
+*/
+
+defined('ABSPATH') || exit;
