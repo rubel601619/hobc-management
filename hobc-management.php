1eb9b97b2a5be4d7a35b812794ce3cd30416c39a
Rubel Mahmud <rubel601619@gmail.com>
2025-05-31 12:33:45 +0600

    update main plugin file.

--- hobc-management.php
+++ hobc-management.php
@@ -18,8 +18,69 @@ require_once plugin_dir_path(__FILE__) . 'inc/class-hobc-admin.php';
 require_once plugin_dir_path(__FILE__) . 'inc/class-hobc-player.php';
 require_once plugin_dir_path(__FILE__) . 'inc/functions.php';
 
-
 // Initialize admin menus
-add_action('plugins_loaded', function() {
-    new HOBC_Admin();
-});
+
+final class HOBC_Management{
+
+    private static $instance;
+
+    public function __construct(){
+
+        // plugin loaded
+        add_action( 'plugins_loaded', [ $this, 'plugin_loaded' ] );
+        
+        // add settings link below the plugin list
+        add_filter( 'plugin_action_links', [ $this, 'add_action_links' ], 10, 2 );
+    }
+
+    /**
+     * initiate the plugin file when it's loaded.
+     * 
+     * @return void
+     */
+    public function plugin_loaded(){
+        new HOBC_Admin();
+    }
+
+    public function add_action_links( $links, $file ){
+        if ( plugin_basename( __FILE__ ) === $file ){
+            $anchor_tag = sprintf(
+                '<a href="%1$s">%2$s</a>',
+                admin_url('admin.php?page=hobc-management'),
+                esc_html__( 'Settings', 'hobc-management' )
+            );
+            array_unshift( $links, $anchor_tag );
+        }
+
+        return $links;
+    }
+    
+
+    /**
+     * create singleton instance
+     *
+     * @return void
+     */
+    public static function init(){
+        if ( is_null( self::$instance ) ){
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
+
+
+/**
+ * initiate the main plufin
+ * 
+ * @return void
+ */
+if( !function_exists( 'hobc_management' ) ){
+
+    function hobc_management(){
+        return HOBC_Management::init();
+    }
+
+    hobc_management();
+}
+
