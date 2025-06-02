454e3af4dbfe110439ac5619b52accfb7e995154
Rubel Mahmud <rubel601619@gmail.com>
2025-06-02 16:10:01 +0600

    add another field for enable or disable pagination options.

--- views/admin/render-general-settings.php
+++ views/admin/render-general-settings.php
@@ -8,6 +8,15 @@
                     <input name="player_per_page" type="number" class="regular-text" value="<?php echo esc_attr(get_option('player_per_page', 10)); ?>">
                 </td>
             </tr>
+            <tr>
+            <th scope="row">Enable Pagination</th>
+            <td>
+                <label class="align-label">
+                    <input type="checkbox" name="hobc_enable_pagination" value="1" <?php checked(1, get_option('hobc_enable_pagination', 1)); ?>>
+                    <span>Yes, enable pagination</span>
+                </label>
+            </td>
+        </tr>
         </tbody>
     </table>
     <?php submit_button(); ?>
